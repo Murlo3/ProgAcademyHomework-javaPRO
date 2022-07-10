3199ece3deb12907218e58a94a9e11172e648667
Murlo3 <vasyl.gromov@gmail.com>
2022-07-10 13:47:37 +0300

    Checker '-' -> '+'

--- L2 - LoginEE/src/main/java/academy/prog/Checker.java
+++ L2 - LoginEE/src/main/java/academy/prog/Checker.java
@@ -4,7 +4,7 @@ public class Checker {
 
     public boolean passChecker(String pass) {
         char[] passArr = pass.toCharArray();
-        if ((passArr.length-1) < 10)
+        if ((passArr.length+1) < 10)
             return false;
         else
             return true;
