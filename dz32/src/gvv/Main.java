2e96239cb481248f52fa3d0c157c537aa4d9dc26
Murlo3 <vasyl.gromov@gmail.com>
2022-07-11 10:50:25 +0300

    dz31 dz32 java PRO

--- dz32/src/gvv/Main.java
+++ dz32/src/gvv/Main.java
@@ -1,8 +1,23 @@
 package gvv;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+	TextContainer one = new TextContainer();
+    Class<?> cls = one.getClass();
+
+    SaveTo saveTo = cls.getAnnotation(SaveTo.class);
+    String path = saveTo.path();
+    Method[] methods = cls.getDeclaredMethods();
+        for (Method sa: methods) {
+            if (sa.isAnnotationPresent(Saver.class)){
+                sa.invoke(one, path);
+                break;
+            }
+        }
+
     }
 }
--- dz32/src/gvv/TextContainer.java
+++ dz32/src/gvv/TextContainer.java
@@ -1,6 +1,19 @@
 package gvv;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+@SaveTo(path = "AAAAA.txt")
 public class TextContainer {
-    private String a = "str";
+    private String a = "strstr";
+
+    @Saver
+    public void saveTextInFile (String path) {
+        try (PrintWriter out = new PrintWriter(path)) {
+            out.println(a);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
